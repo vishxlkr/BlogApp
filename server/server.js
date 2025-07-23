05aca3725d0971171370df784137043c52e9e23d
vishxlkr <vishxlkr@gmail.com>
2025-07-24 03:14:26 +0530

    update

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+
+const app = express();
+
+await connectDB();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("API is working."));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+   console.log("Server is running on PORT " + PORT);
+});
+
+export default app;
