e79217b51ca7d5b75f2098c7062975eed021c36b
vishxlkr <vishxlkr@gmail.com>
2025-07-26 20:46:40 +0530

    update

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -2,6 +2,7 @@ import fs from "fs";
 
 import imagekit from "../configs/imageKit.js";
 import Blog from "../models/Blog.js";
+import Comment from "../models/Comment.js";
 
 export const addBlog = async (req, res) => {
    try {
@@ -48,6 +49,78 @@ export const addBlog = async (req, res) => {
       });
 
       res.json({ success: true, message: "blog added succesfully" });
+   } catch (error) {
+      res.json({ success: false, message: error.message });
+   }
+};
+
+export const getAllBlogs = async (req, res) => {
+   try {
+      const blogs = await Blog.find({ isPublished: true });
+      res.json({ success: true, blogs });
+   } catch (error) {
+      res.json({ success: false, message: error.message });
+   }
+};
+
+export const getBlogById = async (req, res) => {
+   try {
+      const { blogId } = req.params;
+      const blog = await Blog.findById(blogId);
+      if (!blog) {
+         return res.json({ success: false, message: "Blog not found" });
+      }
+      res.json({ success: true, blog });
+   } catch (error) {
+      res.json({ success: false, message: error.message });
+   }
+};
+
+export const deleteBlogById = async (req, res) => {
+   try {
+      const { id } = req.body;
+      await Blog.findByIdAndDelete(id);
+
+      // delete all comments associated with the blog
+      await Comment.deleteMany({ blog: id });
+
+      res.json({ success: true, message: "Blog deleted successfully" });
+   } catch (error) {
+      res.json({ success: false, message: error.message });
+   }
+};
+
+export const togglePublish = async (req, res) => {
+   try {
+      const { id } = req.body;
+      const blog = await Blog.findById(id);
+      blog.isPublished = !blog.isPublished;
+      await blog.save();
+      res.json({ success: true, message: "Blog status updated" });
+   } catch (error) {
+      res.json({ success: false, message: error.message });
+   }
+};
+
+export const addComment = async (req, res) => {
+   try {
+      const { blog, name, content } = req.body;
+      await Comment.create({ blog, name, content });
+      res.json({ success: true, message: "Comment added for review" });
+   } catch (error) {
+      res.json({ success: false, message: error.message });
+   }
+};
+
+export const getBlogComments = async (req, res) => {
+   try {
+      const { blogId } = req.body;
+      const comments = await Comment.find({
+         blog: blogId,
+         isApproved: true,
+      }).sort({ createdAt: -1 });
+
+      res.json({ success: true, comments });
    } catch (error) {
       res.json({ success: false, message: error.message });
    }
