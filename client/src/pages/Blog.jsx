8f639cf5512cfbdf2498bdd145dfc3b8868d5de2
vishxlkr <vishxlkr@gmail.com>
2025-07-28 01:11:33 +0530

    update

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -91,7 +91,7 @@ const Blog = () => {
             </h1>
             <h2 className="my-5 max-w-lg truncate mx-auto">{data.subTitle}</h2>
             <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary">
-               Michael brown
+               Vishal Kumar
             </p>
          </div>
          {/* ------------------------------------------------------ */}
